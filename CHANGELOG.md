734816b413f94e02bfdb46fc992df4f8397f11c4
Abhishek Thakur <34036890+abhishekthakur0@users.noreply.github.com>
2024-01-09 21:16:02 +0530

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 1.0.1
+
+* Minor updates in response to Flutter pub suggestions
---
+++ test/customgauge_new_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:customgauge_new/customgauge_new.dart';
+
+void main() {
+
+}
